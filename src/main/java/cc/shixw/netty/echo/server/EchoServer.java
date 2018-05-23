64befb7d48f35874cb1ca24b6900c2342075abcd
shixianwei <shixianwei@jd.com>
2018-05-23 22:51:42 +0800

    Initial commit

---
+++ src/main/java/cc/shixw/netty/echo/server/EchoServer.java
@@ -0,0 +1,47 @@
+package cc.shixw.netty.echo.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoop;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+public class EchoServer {
+
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+        new EchoServer(20001).start();
+    }
+
+    public void start() throws Exception{
+        final EchoServerHandler echoServerHandler = new EchoServerHandler();
+
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group).channel(NioServerSocketChannel.class).localAddress(new InetSocketAddress(port)).childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel socketChannel) {
+                    socketChannel.pipeline().addLast(echoServerHandler);
+                }
+            });
+
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        }finally {
+            group.shutdownGracefully().sync();
+        }
+
+    }
+}
