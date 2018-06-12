6c11a4d11e35386f1e05ee250c08be579d6e8ca1
shixianwei <shixianwei@jd.com>
2018-06-13 00:54:44 +0800

    java NIO

---
+++ src/main/java/cc/shixw/nio/TimeClient.java
@@ -0,0 +1,12 @@
+package cc.shixw.nio;
+
+
+public class TimeClient {
+
+    public static void main(String[] args) {
+        int port = 8080;
+
+        new Thread(new TimeClientHandle("127.0.0.1",port),"TimeClient-001").start();
+
+    }
+}
