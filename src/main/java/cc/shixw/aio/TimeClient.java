131bf616c4f0c45f4cdfc419ac642e87721b9777
shixianwei <shixianwei@jd.com>
2018-06-16 10:34:34 +0800

    java AIO

---
+++ src/main/java/cc/shixw/aio/TimeClient.java
@@ -0,0 +1,14 @@
+package cc.shixw.aio;
+
+
+import cc.shixw.nio.TimeClientHandle;
+
+public class TimeClient {
+
+    public static void main(String[] args) {
+        int port = 8080;
+
+        new Thread(new AsyncTimeClientHandler("127.0.0.1",port),"AIO-TimeClient-001").start();
+
+    }
+}
